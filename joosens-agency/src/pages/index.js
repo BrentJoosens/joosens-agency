15611328640b96216d98bd315445d302559f8986
Brent Joosens <brent.joosens@student.ap.be>
2022-11-24 12:50:22 +0100

    Gatbsy Plugins finished

--- joosens-agency/src/pages/index.js
+++ joosens-agency/src/pages/index.js
@@ -1,11 +1,15 @@
-import * as React from 'react'
-import Layout from '../components/layout'
+import * as React from "react"
+import Layout from "../components/layout"
+import { StaticImage } from "gatsby-plugin-image"
 
 const IndexPage = () => {
   return (
+    <main>
       <Layout pageTitle="Welcome to Inghelbrecht Agency!">
-      <p>Lorem ipsum</p>
+        <p>Lorem ipsum</p>
+        <StaticImage alt="Een Gatsby astronaut" src="../images/cars.jpg" />
       </Layout>
+    </main>
   )
 }
 
