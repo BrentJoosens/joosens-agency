759e1e26dab88d9ccfe30a354834ace0278d4e76
Brent Joosens <brent.joosens@student.ap.be>
2022-11-25 11:09:47 +0100

    Query data met GraphQL finished

--- joosens-agency/gatsby-config.js
+++ joosens-agency/gatsby-config.js
@@ -16,7 +16,7 @@ module.exports = {
          * De volledige URL van je Headless WordPress site's GraphQL API.
          * Voorbeeld : "https://www.example-site.com/graphql"
          */
-        url: "http://artist-agency.local/graphql",
+        url: " http://joosens-agency.local/graphql",
       },
     },
   ],
--- joosens-agency/src/pages/artists.js
+++ joosens-agency/src/pages/artists.js
@@ -1,10 +1,41 @@
-import * as React from 'react'
-import Layout from '../components/layout'
-const ArtistsPage = () => {
+import * as React from "react"
+import { graphql } from "gatsby"
+import Layout from "../components/layout"
+
+const ArtistsPage = ({
+  data: {
+    allWpArtist: { edges },
+  },
+}) => {
   return (
     <Layout pageTitle="Artists of Inghelbrecht Agency">
-      <p>A list of artists will be displayed here.</p>
+      {edges.map(item => {
+        const artist = item.node.artistMeta
+        return (
+          <p key={item.node.id}>
+            {artist.firstName} {artist.lastName}
+          </p>
+        )
+      })}
     </Layout>
   )
 }
+
+export const query = graphql`
+  query {
+    allWpArtist {
+      edges {
+        node {
+          id
+          artistMeta {
+            firstName
+            lastName
+            artistName
+          }
+        }
+      }
+    }
+  }
+`
+
 export default ArtistsPage
